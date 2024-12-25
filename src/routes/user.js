064c3851a8ed85c77a55ba2402ea5c0b782366b0
LeonardoSaraceli <leonardolodi09@gmail.com>
2024-12-25 12:52:37 +0000

    Feat: add jwt route

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,6 +4,7 @@ import { isTokenValid } from '../middleware/auth.js'
 
 const route = Router()
 
+route.get('/jwt', isTokenValid)
 route.get('/', isTokenValid, getUserCodes)
 route.post('/register', createUser)
 route.post('/login', createToken)
